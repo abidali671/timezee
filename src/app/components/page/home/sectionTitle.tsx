a9a2036805c6e13f219107be06e6068148bf88fb
abidali671 <abid.saeed.ali92@gmail.com>
2025-05-25 18:23:18 +0500

    fix: section title font issue

--- src/app/components/page/home/sectionTitle.tsx
+++ src/app/components/page/home/sectionTitle.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const SectionTitle = ({
     children,
-    className
+    className = '' // Default to an empty string in case no className is passed
 }: any) => {
     return (
         <div className={`relative w-10/12 mx-auto md:mx-0 md:w-5/12 ${className}`}>
-            <h2 className="text-5xl md:text-6xl mb-2 text-white italic tracking-tight  ">
+            <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl mb-2 text-white italic tracking-tight">
                 {children}
             </h2>
-            <hr className=" w-auto text-red-300 border-1 md:border-3 border-yellow-400 relative bottom-4 " />
+            <hr className="w-auto text-red-300 border-1 md:border-3 border-yellow-400 relative bottom-4" />
         </div>
     )
 }
 
-export default SectionTitle
+export default SectionTitle;
