8f29c12009c8854de78b5c1469836833a59f45f4
abidali671 <abid.saeed.ali92@gmail.com>
2025-06-03 16:59:52 +0500

    feat: dynamic colors

--- src/app/components/CartToggle.tsx
+++ src/app/components/CartToggle.tsx
@@ -2,13 +2,13 @@
 import { ShoppingCart } from 'lucide-react';
 import { useCart } from '@/context/CartContext';
 
-export default function CartToggle() {
+export default function CartToggle({ color }: { color?: string }) {
     const { cart, toggleCart } = useCart();
     const itemCount = cart.reduce((total, item) => total + item.quantity, 0);
 
     return (
         <div className="relative cursor-pointer" onClick={toggleCart}>
-            <ShoppingCart size={20} />
+            <ShoppingCart size={20} color={color} />
             {itemCount > 0 && (
                 <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs  rounded-full w-3 h-3 flex items-center justify-center">
                     {itemCount}
