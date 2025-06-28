96e2ab0150120981e0dc296fe58ce91a79e7d2ed
abidali671 <abid.saeed.ali92@gmail.com>
2025-06-28 21:40:41 +0500

    fix: image url

--- src/app/components/cart.tsx
+++ src/app/components/cart.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 
 export default function CartSidebar() {
     const { cart, dispatch, isOpen, toggleCart } = useCart();
-    console.log(cart, 'cart');
+    console.log(cart, 'cart===');
 
     return (
         <div
@@ -15,7 +15,7 @@ export default function CartSidebar() {
                 }`}
         >
             <div className="p-4 flex justify-between items-center">
-                {cart.length > 0 && <h1 className='text-4xl text-white whitespace-nowrap'>Your cart  </h1>}
+                {cart.length > 0 && <h1 className='text-4xl text-white whitespace-nowrap'>Your cart</h1>}
                 <button onClick={toggleCart} className='text-2xl cursor-pointer'>✕</button>
             </div>
             <div className="p-4 space-y-4 overflow-y-auto h-[calc(100%-180px)]">
@@ -29,20 +29,21 @@ export default function CartSidebar() {
                     cart.map(item => (
                         <div key={item.slug} className="grid grid-cols-2  w-8/12 gap-4">
                             <div>
-                                <Image src={item.image?.fields?.file?.url}
-                                    alt={item.name} height={100} width={100} />
+                                <Image src={item.imageUrl || 'https:' + item.image?.fields?.file?.url}
+                                    alt={item.name || item.title} height={100} width={100} />
                             </div>
                             <div className='grid gap-2'>
                                 <p>{item.brandName}</p>
-                                <h1 className='text-md text-white whitespace-nowrap '>{item.name}</h1>
+                                <h1 className='text-md text-white whitespace-nowrap '>{item.name || item.title}</h1>
                                 <p>PKR {(item.stock * item.price).toFixed(2)}</p>
                                 <div className='flex items-center space-x-2'>
                                     {/* Decrease quantity */}
                                     <button
                                         className="text-sm border flex items-center justify-center w-8 h-8 rounded"
-                                        onClick={() =>
-                                            dispatch({ type: 'DECREASE_QUANTITY', payload: item.slug })
-                                        }
+                                        onClick={() => {
+                                            dispatch({ type: 'DECREASE_QUANTITY', payload: item.slug });
+
+                                        }}
                                         disabled={item.stock <= 1}
                                     >
                                         -
@@ -53,9 +54,11 @@ export default function CartSidebar() {
                                     {/* Increase quantity */}
                                     <button
                                         className="text-sm  border flex items-center justify-center w-8 h-8 rounded"
-                                        onClick={() =>
-                                            dispatch({ type: 'INCREASE_QUANTITY', payload: item.slug })
-                                        }
+                                        onClick={() => {
+                                            dispatch({ type: 'INCREASE_QUANTITY', payload: item.slug });
+
+                                        }}
+                                        disabled={item.stock == item.inStock}
                                     >
                                         +
                                     </button>
@@ -76,26 +79,28 @@ export default function CartSidebar() {
                 )}
             </div>
 
-            {cart.length > 0 && (
-                <div className='flex justify-center items-center flex-col'>
-                    <Link href="/checkout" passHref>
-                        <AnimatedButton
-                            className="w-full text-sm"
-                            onClick={toggleCart}
-                        >
-                            Proceed to Checkout
-                        </AnimatedButton>
-                    </Link>
-                    <div className="p-4 border-t w-full">
-                        <button
-                            className="w-full bg-black text-white py-2 rounded cursor-pointer hover:bg-white hover:text-black transition-colors"
-                            onClick={() => dispatch({ type: 'CLEAR_CART' })}
-                        >
-                            Clear Cart
-                        </button>
+            {
+                cart.length > 0 && (
+                    <div className='flex justify-center items-center flex-col'>
+                        <Link href="/checkout" passHref>
+                            <AnimatedButton
+                                className="w-full text-sm"
+                                onClick={toggleCart}
+                            >
+                                Proceed to Checkout
+                            </AnimatedButton>
+                        </Link>
+                        <div className="p-4 flex justify-center items-center  w-full">
+                            <button
+                                className="   w-8/12 bg-black text-white py-2 rounded cursor-pointer hover:bg-white hover:text-black transition-colors"
+                                onClick={() => dispatch({ type: 'CLEAR_CART' })}
+                            >
+                                Clear Cart
+                            </button>
+                        </div>
                     </div>
-                </div>
-            )}
-        </div>
+                )
+            }
+        </div >
     );
 }
