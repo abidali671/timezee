24fab9fa5708a5bdb84556103ea56dadbd29a4e6
abidali671 <abid.saeed.ali92@gmail.com>
2025-07-11 00:15:48 +0500

    feat: update charts

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,23 @@
-// app/dashboard/page.tsx
+'use client'
+import { useOrders } from '@/context/orderContext';
+import { useProducts } from '@/context/productsContext';
+
 export default function DashboardHome() {
+    const { orders } = useOrders();
+    const { products } = useProducts();
+console.log('Orders:', orders);
+
     return (
         <div className="space-y-6">
             <h2 className="text-3xl font-semibold text-gray-800">Dashboard</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 <div className="bg-white p-6 rounded-lg shadow-md">
                     <h3 className="text-xl font-bold text-gray-700">Total Products</h3>
-                    <p className="text-2xl text-gray-600">30</p>
+                    <p className="text-2xl text-gray-600">{products.length}</p>
                 </div>
                 <div className="bg-white p-6 rounded-lg shadow-md">
                     <h3 className="text-xl font-bold text-gray-700">Total Orders</h3>
-                    <p className="text-2xl text-gray-600">100</p>
+                    <p className="text-2xl text-gray-600">{orders.length}</p>
                 </div>
                 <div className="bg-white p-6 rounded-lg shadow-md">
                     <h3 className="text-xl font-bold text-gray-700">Revenue</h3>
