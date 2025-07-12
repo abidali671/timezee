53276fbdbb785f0d37bf10b602d5d5f85d046cce
abidali671 <abid.saeed.ali92@gmail.com>
2025-07-12 20:44:12 +0500

    feat: update dashboard charts

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,78 @@
 'use client'
-import { useOrders } from '@/context/orderContext';
 import { useProducts } from '@/context/productsContext';
+import { Line } from 'react-chartjs-2';
+import { useOrdersHook } from '../../hooks/useOrdersHook';
+import { Boxes, ChartNoAxesColumn, PackageCheck } from 'lucide-react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 export default function DashboardHome() {
-    const { orders } = useOrders();
     const { products } = useProducts();
-console.log('Orders:', orders);
+    const { totalOrders, totalRevenue, totalProfit, loading, chartData } = useOrdersHook();
+    if (loading) return <div>Loading...</div>;
+    const chartConfig = {
+        labels: chartData.map((d) => d.date),
+        datasets: [
+            {
+                label: 'Revenue',
+                data: chartData.map((d) => d.totalRevenue),
+                borderColor: 'green',
+                fill: false,
+            },
+            {
+                label: 'Orders',
+                data: chartData.map((d) => d.orderCount),
+                borderColor: 'blue',
+                fill: false,
+            },
+        ],
+    };
+
 
     return (
         <div className="space-y-6">
-            <h2 className="text-3xl font-semibold text-gray-800">Dashboard</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            <h2 className="text-3xl font-semibold text-gray-800"> Dashboard</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
                 <div className="bg-white p-6 rounded-lg shadow-md">
-                    <h3 className="text-xl font-bold text-gray-700">Total Products</h3>
+                    <div className='flex gap-x-3'>
+                        <Boxes />
+                        <h3 className="text-2xl font-bold text-gray-700">Total Products</h3>
+                    </div>
+
                     <p className="text-2xl text-gray-600">{products.length}</p>
                 </div>
                 <div className="bg-white p-6 rounded-lg shadow-md">
-                    <h3 className="text-xl font-bold text-gray-700">Total Orders</h3>
-                    <p className="text-2xl text-gray-600">{orders.length}</p>
+                    <div className='flex gap-x-3'>
+                        <PackageCheck />
+                        <h3 className="text-2xl font-bold text-gray-700">Total Orders</h3>
+
+                    </div>
+                    <p className="text-2xl text-gray-600">{totalOrders}</p>
                 </div>
                 <div className="bg-white p-6 rounded-lg shadow-md">
-                    <h3 className="text-xl font-bold text-gray-700">Revenue</h3>
-                    <p className="text-2xl text-gray-600">$5,000</p>
+                    <div className='flex gap-x-3'>
+                        <ChartNoAxesColumn />
+                        <h3 className="text-2xl font-bold text-gray-700">Revenue</h3>
+                    </div>
+
+                    <p className="text-2xl text-gray-600">PKR {totalRevenue}</p>
                 </div>
+                <div className="bg-white p-6 rounded-lg shadow-md">
+                    <h3 className="text-2xl font-bold text-gray-700">% Profit</h3>
+                    <p className="text-2xl text-gray-600">PKR {totalProfit}</p>
+                </div>
+            </div>
+            <div className="chart-container w-6/12 bg-white p-6 rounded-lg shadow-md">
+                <Line data={chartConfig} />
             </div>
         </div>
     );
