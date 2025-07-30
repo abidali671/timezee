9bd2f45fb4039d19dc88f2e12b42d853f4ccc636
abidali671 <abid.saeed.ali92@gmail.com>
2025-07-30 15:51:02 +0500

    feat: dynamic & static sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,32 +6,31 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
     const staticPages = [
         {
-            url: 'timezee-five.vercel.app',
+            url: 'https://timezee-five.vercel.app',
             lastModified: new Date(),
-            changeFrequency: "yearly" as const,
+            changeFrequency: 'yearly' as const,
             priority: 1,
         },
         {
-            url: 'timezee-five.vercel.app/about',
+            url: 'https://timezee-five.vercel.app/shop',
             lastModified: new Date(),
-            changeFrequency: "monthly" as const,
+            changeFrequency: 'monthly' as const,
             priority: 0.8,
         },
         {
-            url: 'timezee-five.vercel.app/blog',
+            url: 'https://timezee-five.vercel.app/contact',
             lastModified: new Date(),
-            changeFrequency: "weekly" as const,
+            changeFrequency: 'weekly' as const,
             priority: 0.5,
         },
     ];
 
     const productPages = products.map((product: any) => ({
         url: `https://timezee-five.vercel.app/product/${product.slug}`,
-        changeFrequency: "weekly" as const,
+        changeFrequency: 'weekly' as const,
         lastModified: new Date().toISOString(),
         images: product.imageUrl ? [product.imageUrl] : [],
     }));
 
     return [...staticPages, ...productPages];
-
 }
