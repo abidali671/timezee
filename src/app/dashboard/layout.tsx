47650c81f36cf38c2d22f35a43230686050695a7
abidali671 <abid.saeed.ali92@gmail.com>
2025-07-30 15:51:52 +0500

    feat: meta data for dashboard pages

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,18 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import DashboardLayout from './DashboardLayout';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: {
+        default: "Dashboard | SwissTime",
+        template: "%s | SwissTime",
+    },
+    robots: {
+        index: false,
+        follow: false,
+    },
+};
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
     const cookieStore = cookies();
